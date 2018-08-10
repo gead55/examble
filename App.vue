808551f56e875ad19405a471d546abe935088683
atthaporn <atthaporns@becicorp.com>
2018-08-10 17:16:54 +0700

    up

---
+++ App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1 @click="onClick">
+    {{message}}
+  </h1>
+</template>
+
+<script>
+import Vue from "vue"
+import { Component, Prop } from "vue-property-decorator"
+
+@Component({})
+export default class App extends Vue {
+  @Prop({ default: "Hello from Prop decorator" })
+  message
+
+  onClick() {
+    this.message = "Goodbye"
+  }
+}
+</script>
